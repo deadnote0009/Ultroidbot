c92f7ef435d8f520a3eb03a15544304960b3b516
DARK NIL <92631979+nahid-nil@users.noreply.github.com>
2023-02-03 23:06:22 +0600

    Update _on_adds.py

--- assistant/manager/_on_adds.py
+++ assistant/manager/_on_adds.py
@@ -19,5 +19,5 @@ async def dueha(e):
     if sm == "OFF":
         return
     if not sm:
-        sm = "Thanks for Adding me :)"
+        sm = "ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ :)"
     await e.reply(sm, link_preview=False)
