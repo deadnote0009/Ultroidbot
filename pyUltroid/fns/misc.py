028848d3328a09d85463332a333831f6d416c7af
DARK NIL <92631979+nahid-nil@users.noreply.github.com>
2023-02-28 18:50:37 +0600

    Update misc.py

--- pyUltroid/fns/misc.py
+++ pyUltroid/fns/misc.py
@@ -165,8 +165,8 @@ async def allcmds(event, telegraph):
         for zz in LIST[z]:
             txt += HNDLR + zz + "\n"
         txt += "\n\n"
-    t = telegraph.create_page(title="Ultroid All Cmds", content=[txt])
-    await eor(event, f"All Ultroid Cmds : [Click Here]({t['url']})", link_preview=False)
+    t = telegraph.create_page(title="★  © 🇧🇩 Uʟᴛʀᴏɪᴅ✘Sᴜᴘʀᴇᴍᴀᴄʏ 🥰", content=[txt])
+    await eor(event, f"★★ READ ᴜꜱᴇʀʙᴏᴛ all cmds : [Click Here]({t['url']})", link_preview=False)
 
 
 async def ReTrieveFile(input_file_name):
